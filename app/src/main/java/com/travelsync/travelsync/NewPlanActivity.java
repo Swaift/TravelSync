29c558154d077401153c1b3055327f23c6de3721
Christian Sweet <christian.t.sweet@hotmail.com>
2017-07-15 18:59:24 -0500

    Updated styles and interface design

--- app/src/main/java/com/travelsync/travelsync/NewPlanActivity.java
+++ app/src/main/java/com/travelsync/travelsync/NewPlanActivity.java
@@ -1,23 +1,38 @@
 package com.travelsync.travelsync;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 public class NewPlanActivity extends AppCompatActivity {
     Intent intent;
+    Intent previous;
     Button b1;
-    String[] contact_array = {"Android","IPhone","WindowsMobile","Blackberry",
-            "WebOS","Ubuntu","Windows7","Max OS X"};
+    Button back;
+    String[] contact_array;
+    ListView listView;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_plan);
         intent = new Intent(this, Plan.class);
+        previous = new Intent(this, Home.class);
+        editText = (EditText) findViewById(R.id.Title);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText("");
+            }
+        });
 
         b1=(Button)findViewById(R.id.button);
         b1.setOnClickListener(new View.OnClickListener() {
@@ -26,12 +41,27 @@ public class NewPlanActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        ListView listView = (ListView) findViewById(R.id.contact_list);
-        String[] values = new String[] { "Alex", "Christian", "Tawsif",
-                "Brendan", "Dan", "John" };
+        back=(Button)findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(previous);
+            }
+        });
+        listView = (ListView) findViewById(R.id.contact_list);
+        contact_array = new String[] { "Alex", "Christian", "Tawsif",
+                "Brendan", "Dan", "John", "Negin", "Divya", "Shubhangi", "Jake", "Patrick" };
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, values);
+                android.R.layout.simple_list_item_1, contact_array);
         listView.setAdapter(adapter);
-    }
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                String selected = contact_array[position];
+                //listView.setItemChecked();
+                listView.setItemChecked(position+1, true);
+            }
+        });
+    }
 }
--- app/src/main/java/com/travelsync/travelsync/Plan.java
+++ app/src/main/java/com/travelsync/travelsync/Plan.java
@@ -9,10 +9,21 @@ import android.widget.CalendarView;
 
 public class Plan extends AppCompatActivity {
     CalendarView calendar;
+    Intent previous;
+    Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan);
+        previous = new Intent(this, NewPlanActivity.class);
+
+        back=(Button)findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(previous);
+            }
+        });
 
         calendar=(CalendarView)findViewById(R.id.calendarView);
     }
