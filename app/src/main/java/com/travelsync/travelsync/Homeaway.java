4340ce248df99a771862f9676c2cf078379a1b21
Christian Sweet <christian.t.sweet@hotmail.com>
2017-07-15 19:47:05 -0500

    Added a homeaway activity

--- app/src/main/java/com/travelsync/travelsync/Homeaway.java
+++ app/src/main/java/com/travelsync/travelsync/Homeaway.java
@@ -7,8 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 
-public class Plan extends AppCompatActivity {
-    CalendarView calendar;
+
+public class Homeaway extends AppCompatActivity{
     Intent intent;
     Intent previous;
     Button b1;
@@ -16,25 +16,23 @@ public class Plan extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_plan);
+        setContentView(R.layout.activity_homeaway);
         intent = new Intent(this, Homeaway.class);
-        previous = new Intent(this, NewPlanActivity.class);
+        previous = new Intent(this, Plan.class);
 
-        b1=(Button)findViewById(R.id.button1);
+        b1 = (Button) findViewById(R.id.button1);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(intent);
+                //startActivity(intent);
             }
         });
-        back=(Button)findViewById(R.id.back);
+        back = (Button) findViewById(R.id.back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(previous);
             }
         });
-
-        calendar=(CalendarView)findViewById(R.id.calendarView);
     }
 }
