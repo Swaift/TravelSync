6b7accdba242aa58c3e368745c45f506504e096b
Tawsif <tawsifazad@gmail.com>
2017-07-15 20:22:01 -0500

    Implement Chrome custom tab

--- app/src/main/java/com/travelsync/travelsync/Homeaway.java
+++ app/src/main/java/com/travelsync/travelsync/Homeaway.java
@@ -1,11 +1,14 @@
 package com.travelsync.travelsync;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.customtabs.CustomTabsIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.EditText;
 
 
 public class Homeaway extends AppCompatActivity{
@@ -13,17 +16,33 @@ public class Homeaway extends AppCompatActivity{
     Intent previous;
     Button b1;
     Button back;
+    String city  = ((EditText) findViewById(R.id.cityInput )).getText().toString();
+    String state = ((EditText) findViewById(R.id.stateInput)).getText().toString();
+    String min   = ((EditText) findViewById(R.id.minInput  )).getText().toString();
+    String max   = ((EditText) findViewById(R.id.maxInput  )).getText().toString();
+    String bed   = ((EditText) findViewById(R.id.bedInput  )).getText().toString();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homeaway);
         intent = new Intent(this, Homeaway.class);
         previous = new Intent(this, Plan.class);
+        final AppCompatActivity homeaway = this;
 
         b1 = (Button) findViewById(R.id.button1);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String url = "https://www.homeaway.com/results/keywords:" +
+                        city + "+" +
+                        state + "/minNightlyPrice/" +
+                        min + "/maxNightlyPrice/" +
+                        max + "/minSleeps/" +
+                        bed;
+                CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+                CustomTabsIntent customTabsIntent = builder.build();
+                customTabsIntent.launchUrl(homeaway, Uri.parse(url));
                 //startActivity(intent);
             }
         });
